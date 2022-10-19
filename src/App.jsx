3f5aaf32dc7e47714135745dd88023ae38968fe6
vladian95 <kvankvan1995@gmail.com>
2022-10-19 16:22:46 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100vh;
+`;
+function App() {
+  return <Container>hello vlad</Container>;
+}
+
+export default App;
