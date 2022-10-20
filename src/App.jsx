66a87dbd060fce81ad47abe571cd705cc336380f
vladian95 <kvankvan1995@gmail.com>
2022-10-20 09:02:00 +0200

    added features

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
 import Feature from './components/Feature';
@@ -6,17 +6,28 @@ import Feature from './components/Feature';
 const Container = styled.div`
   height: 100vh;
   overflow: hidden;
+  position: relative;
 `;
 
-const IntoShape = styled.div`
+const Shape = css`
   width: 100%;
   height: 100%;
   position: absolute;
   top: 0px;
   left: 0px;
+  z-index: -1;
+`;
+
+const IntoShape = styled.div`
+  ${Shape}
   clip-path: polygon(1000% 0%, 66% 0%, 45% 100%, 100% 100%);
   background-color: crimson;
-  z-index: -1;
+`;
+
+const FeatureShape = styled.div`
+  ${Shape}
+  clip-path: polygon(0 0, 45% 0, 37% 100%, 0% 100%);
+  background-color: green;
 `;
 
 function App() {
@@ -29,6 +40,7 @@ function App() {
       </Container>
       <Container>
         <Feature />
+        <FeatureShape />
       </Container>
     </>
   );
--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,28 +1,82 @@
 import React from 'react';
 import styled from 'styled-components';
-import Phone from '../img/phone.png';
+import Flower from '../img/flower.png';
+import AnimatedShapes from './AnimatedShapes';
 
 const Container = styled.div`
   display: flex;
+  align-items: center;
 `;
 
 const Left = styled.div`
   width: 50%;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 70%;
+`;
 
 const Right = styled.div`
   width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 180px;
+`;
+
+const Title = styled.span`
+  font-size: 70px;
+`;
+
+const SubTitle = styled.span`
+  font-size: 24px;
+  font-style: italic;
+  color: #333;
+  margin-top: 30px;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: #777;
+  margin-top: 30px;
+`;
+
+const Button = styled.button`
+  width: 150px;
+  background-color: darkblue;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 20px;
+  font-size: 20px;
+  color: #fff;
+  cursor: pointer;
+  margin-top: 20px;
 `;
 
 const Feature = () => {
   return (
     <Container>
       <Left>
-        <Image src={Phone} />
+        <Image src={Flower} />
       </Left>
-      <Right></Right>
+      <Right>
+        <Title>
+          <b>good</b> website
+          <br />
+          <b>good</b> business
+        </Title>
+        <SubTitle>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </SubTitle>
+        <Desc>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum et
+          consequuntur deserunt excepturi? Similique itaque labore perspiciatis
+          at. Consequuntur quos ullam animi itaque, est perferendis qui culpa
+          consequatur eligendi. Illo.
+        </Desc>
+        <Button>Learn More</Button>
+      </Right>
+      <AnimatedShapes />
     </Container>
   );
 };
